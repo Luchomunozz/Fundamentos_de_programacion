8e47eb68e85acfcbe076ccc1b0648ced759cabf3
Luchomunozz <lmmunoz311@misena.edu.co>
2021-10-06 13:19:41 -0500

    Se agregó una nueva carpeta

---
+++ suma2/suma2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace suma2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese un numero, el cual será sumado por 23");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int numero2 = 23;
+
+            int resultado = numero + numero2;
+
+            Console.WriteLine("el resultado de la suma es:" + Convert.ToString(resultado));
+
+        }
+    }
+}
